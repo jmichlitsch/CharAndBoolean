a8178fed6f2f911235a362401e689d26cb41a8f7
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-09-09 12:56:37 -0600

    inital commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,22 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char myChar = 'D';
+        char myUniCodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUniCodeChar);
+
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBoolean = true;
+        boolean myFalseBoolean = false;
+        System.out.println(myTrueBoolean);
+        System.out.println(myFalseBoolean);
+
+        boolean isCustomerOverTwentyOne = true;
+        
+    }
+}
